104580a35bb32b48f99a6982f01dc49726a2bc57
marceloquevedo <marceloquevedo@mbp-de-marcelo>
2024-11-24 20:43:08 -0300

    Primeiros ajustes

---
+++ src/main/java/com/br/fiap/postech/soat7grupo5_producao/Soat7grupo5ProducaoApplication.java
@@ -0,0 +1,13 @@
+package com.br.fiap.postech.soat7grupo5_producao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Soat7grupo5ProducaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Soat7grupo5ProducaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/fiap/postech/soat7grupo5_producao/Soat7grupo5ProducaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.fiap.postech.soat7grupo5_producao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class Soat7grupo5ProducaoApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
