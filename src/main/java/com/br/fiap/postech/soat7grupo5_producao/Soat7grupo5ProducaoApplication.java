af3609db66b0e783cc8e3cda79e2d5b167a47e4f
Bruno Bucci <bruno@brunobucci.com>
2024-12-02 21:34:03 -0300

    Update Soat7grupo5ProducaoApplication.java

--- src/main/java/com/br/fiap/postech/soat7grupo5_producao/Soat7grupo5ProducaoApplication.java
+++ src/main/java/com/br/fiap/postech/soat7grupo5_producao/Soat7grupo5ProducaoApplication.java
@@ -2,14 +2,11 @@ package com.br.fiap.postech.soat7grupo5_producao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 @OpenAPIDefinition(info = 
 @Info(title = "Soat7grupo5 API - Produção", 
 	version = "1.0", 
